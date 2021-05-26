16d46fba1b79d866f9eb67c138c1aecfb8397677
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2021-05-26 10:26:26 +0100

    static files

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
+var date = new Date();
+var elDate = document.querySelector('#date');
+elDate.innerHTML = date.getFullYear();
+
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
